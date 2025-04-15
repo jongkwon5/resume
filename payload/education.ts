a6587d8b5f62d0258b328e461a1e00163bc3b7d4
Parkjongkwon <jongkwong@naver.com>
2025-04-15 20:19:28 +0900

    250415 PJK

--- payload/education.ts
+++ payload/education.ts
@@ -5,13 +5,13 @@ const education: IEducation.Payload = {
 
   list: [
     {
-      title: '건국대학교 교육대학원 음악교육전공(석사)',
+      title: '건국대학교 교육대학원 교육학 석사(음악교육전공) 졸업',
       subTitle: '졸업 학점 (4.2 / 4.5)',
       startedAt: '20-03-01',
       endedAt: '22-08-22',
     },
     {
-      title: '청운대학교 실용음악과(학사)',
+      title: '청운대학교 실용음악과 학사 졸업',
       subTitle: '졸업 학점 (3.49 / 4.5)',
       startedAt: '13-03-01',
       endedAt: '19-02-13',
