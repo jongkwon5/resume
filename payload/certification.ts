45874ea1352cf4e3f745abb3df443d2d5d045483
jkpark <jkpark@lineupkorea.com>
2025-12-16 20:45:20 +0900

    feat: 자격증 추가 2025-12-16

--- payload/certification.ts
+++ payload/certification.ts
@@ -5,6 +5,11 @@ const Certification: ICertification.Payload = {
 
   list: [
     {
+      title: '리눅스마스터 2급',
+      subTitle: '한국정보통신진흥협회',
+      at: '26-01-02',
+    },
+    {
       title: '네트워크관리사 2급',
       subTitle: '한국정보통신자격협회',
       at: '25-09-16',
@@ -30,29 +35,14 @@ const Certification: ICertification.Payload = {
       at: '16-05-14',
     },
     {
-      title: '정보기술자격(ITQ) 인터넷 A등급 ',
+      title: '정보기술자격(ITQ) OA Master',
       subTitle: '한국생산성본부(KPC)',
-      at: '10-06-01',
-    },
-    {
-      title: '정보기술자격(ITQ) 한글엑셀 A등급 ',
-      subTitle: '한국생산성본부(KPC)',
-      at: '06-11-09',
+      at: '07-02-01',
     },
     {
       title: '컴퓨터활용능력 2급  ',
       subTitle: '대한상공회의소',
       at: '06-08-08',
-    },
-    {
-      title: '정보기술자격(ITQ) 한글파워포인트 A등급 ',
-      subTitle: '한국생산성본부(KPC)',
-      at: '05-05-10',
-    },
-    {
-      title: '정보기술자격(ITQ) 아래한글 A등급 ',
-      subTitle: '한국생산성본부(KPC)',
-      at: '05-01-11',
     },
     {
       title: '워드프로세서 1급 ',
